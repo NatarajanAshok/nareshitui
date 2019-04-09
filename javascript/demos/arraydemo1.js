0ba7d686d9a03c299d49c63407d2deda78cf33b5
suhvishal <23210008@mkcl.org>
2019-04-09 06:56:43 +0530

    -;

---
+++ javascript/demos/arraydemo1.js
@@ -0,0 +1,39 @@
+
+// var numsArr = [ 10, 20, 30, 40, 50 ];
+
+// numsArr.forEach(function(num){
+//     console.log(num)
+// })
+
+// var namesArr = ['Vishal','Balaji','Gurunath','Ravi'];
+
+// namesArr.sort();
+
+// console.log(namesArr)
+
+
+// var newArr = namesArr.map(function(item){
+//     return item.toUpperCase();
+// })
+//console.log(newArr)
+
+
+var nums = [21,54,2,3,43,65,7,54,3];
+nums.sort(function(a, b){
+    return a-b;
+});
+console.log(nums)
+
+
+
+
+
+// console.log('length of numsArr = ' + numsArr.length)
+
+// numsArr.push(60);
+
+// console.log(numsArr)
+
+// var ele = numsArr.pop();
+// console.log('removed item = ' + ele)
+// console.log(numsArr)
---
+++ javascript/demos/vardemo.js
@@ -0,0 +1,15 @@
+var a = 10;
+console.log('variable a  = ' + a)
+console.log('type of variable a = ' + typeof(a))
+
+a = 'Welcome'
+console.log('variable a  = ' + a)
+console.log('type of variable a = ' + typeof(a))
+
+a = {
+    fname : 'vishal',
+    lname : 'shah'
+}
+
+console.log(a)
+console.log('type of variable a = ' + typeof(a))
